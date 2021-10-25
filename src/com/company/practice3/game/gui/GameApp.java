c725f97ebb6b07ea0dbd187c2e96c4f6366f3a8a
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2021-10-25 19:33:13 +0300

    add icon

--- src/com/company/practice3/game/gui/GameApp.java
+++ src/com/company/practice3/game/gui/GameApp.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +22,7 @@ public class GameApp extends Application{
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Practice 3 game");
+        stage.getIcons().add(new Image(GameApp.class.getResourceAsStream("icon.png")));
         stage.show();
     }
     @Override
