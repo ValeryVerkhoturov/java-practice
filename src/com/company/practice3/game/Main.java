262347fc3bee2a32deb3445f4cc735c346c77382
Валерий <70258211+ValeryVerkhoturov@users.noreply.github.com>
2021-11-08 19:15:25 +0300

    Update Main.java

--- src/com/company/practice3/game/Main.java
+++ src/com/company/practice3/game/Main.java
@@ -4,7 +4,6 @@ import com.company.practice3.game.gui.GameApp;
 
 public class Main {
     public static void main(String[] args) {
-        GameApp gameApp = new GameApp();
-        gameApp.main(args);
+        GameApp.main(args);
     }
 }
